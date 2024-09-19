633fcf2b2e106489dc8bde3121eae70de66d6fb6
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2024-09-19 16:56:19 +0700

    setup: backend folder and API

---
+++ be-cat-service/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const process = require("process");
+const app = express();
+
+// DOTENV CONFIG
+dotenv.config();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// CORS
+app.use(cors());
+
+// MONGODB CONNECTION
+if (!process.env.MONGO_URL) {
+    throw Error("Database connection string not found");
+}
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+    console.log("Succesfully connected to MongoDB");
+    })
+    .catch((err) => {
+    console.log("Failed to connect to MongoDB");
+    console.log(err);
+    });
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("Hello from PAW Backend Service!");
+    });
+
+// APP START
+app.listen(5000, () => {
+    console.log("Server is running on http://localhost:5000");
+    });
